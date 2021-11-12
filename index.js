26f35b2bc31808e8df8662d22d591c18741792ff
Ana C <apspfc@gmail.com>
2021-11-12 16:04:34 +0000

    unique

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
-
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
@@ -18,53 +15,46 @@ mongoose
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
     updateDatabase();
-    
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
- 
 
-
-  async function updateDatabase() {
-    try {
-      
-      const createdRecipe = await Recipe.create({
+async function updateDatabase() {
+  try {
+    const createdRecipe = await Recipe.create({
       title: "first recipe",
       cuisine: "first recipe",
     });
     console.log(createdRecipe.title);
 
-    await Recipe.insertMany(data,()=>{
-      data.map((recipe)=>{
+    await Recipe.insertMany(data, () => {
+      data.map((recipe) => {
         console.log(recipe.title);
-        uploaded = true;
       });
     });
 
-
-    
-    
-    await Recipe.deleteOne({ title : "Carrot Cake"});
+    await Recipe.deleteOne({ title: "Carrot Cake" });
     console.log("deleted");
-    await Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},{duration:100});
+    await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
     console.log("changed");
-    await Recipe.deleteOne({ title : "Carrot Cake"});
+    await Recipe.deleteOne({ title: "Carrot Cake" });
     console.log("deleted");
-    
-    } catch(e){
-      console.log(e, "an error occurred");
-    } finally {
-      mongoose.connection.close();
-    }
+  } catch (e) {
+    console.log(e, "an error occurred");
+  } finally {
+    mongoose.connection.close();
+    console.log("Connection Closed!");
   }
-
- 
+}
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   title: {
     type: String,
     required: true,
+    unique: true,
   },
   level: {
     type: String,
-    enum: ["Easy Peasy","Amateur Chef","UltraPro Chef"],
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
   },
   ingridients: [String],
   cuisine: {
@@ -17,23 +18,31 @@ const recipeSchema = new Schema({
   },
   dishType: {
     type: String,
-    enum: ["breakfast","main_course","soup","snack","drink","dessert","other"],
+    enum: [
+      "breakfast",
+      "main_course",
+      "soup",
+      "snack",
+      "drink",
+      "dessert",
+      "other",
+    ],
   },
   image: {
     type: String,
-    default: "https://images.media-allrecipes.com/images/75131.jpg"
-    },
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
   duration: {
     type: Number,
     min: 0,
   },
   creator: String,
-  created:{
+  created: {
     type: Date,
     timestamps: true,
   },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
